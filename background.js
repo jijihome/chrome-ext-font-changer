78fd0484a6054ca548e72735a1de2e5356195d4e
jijihome <jijihome@gmail.com>
2024-09-02 23:32:28 +0800

    修复了代码中的调试语句，优化了字体设置器的逻辑判断。

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("settings.html"),
-  });
-});
+// chrome.action.onClicked.addListener(() => {
+//   chrome.tabs.create({
+//     url: chrome.runtime.getURL("settings.html"),
+//   });
+// });
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("字体设置器扩展已安装");
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log("字体设置器扩展已安装");
+// });
--- content.js
+++ content.js
@@ -1,25 +1,21 @@
-console.log("字体设置器 content script 已注入");
+// console.log("字体设置器 content script 已注入");
 
-chrome.storage.sync.get(
-  ["enableAllSites", "allowedUrls", "excludedUrls", "font"],
-  ({ enableAllSites, allowedUrls, excludedUrls, font }) => {
-    console.log("从存储中加载的设置:", {
-      enableAllSites,
-      allowedUrls,
-      excludedUrls,
-      font,
-    });
+chrome.storage.sync.get(["enableAllSites", "allowedUrls", "excludedUrls", "font"], ({ enableAllSites, allowedUrls, excludedUrls, font }) => {
+  // console.log("从存储中加载的设置:", {
+  //   enableAllSites,
+  //   allowedUrls,
+  //   excludedUrls,
+  //   font,
+  // });
 
-    const currentUrl = window.location.href;
-    const isAllowed =
-      enableAllSites || allowedUrls.some((url) => currentUrl.includes(url));
-    const isExcluded = excludedUrls.some((url) => currentUrl.includes(url));
+  const currentUrl = window.location.href;
+  const isAllowed = enableAllSites || allowedUrls.some((url) => currentUrl.includes(url));
+  const isExcluded = excludedUrls.some((url) => currentUrl.includes(url));
 
-    if (isAllowed && !isExcluded && font) {
-      console.log("应用字体:", font);
-      document.body.style.fontFamily = font;
-    } else {
-      console.log("当前页面不符合应用字体的条件");
-    }
+  if (isAllowed && !isExcluded && font) {
+    // console.log("应用字体:", font);
+    document.body.style.fontFamily = font;
+  } else {
+    // console.log("当前页面不符合应用字体的条件");
   }
-);
+});
